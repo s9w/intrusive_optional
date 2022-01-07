e2dc2be1a1da824104311266d57b2d62f1137aac
Sebastian Werhausen <swerhausen@gmail.com>
2022-01-07 00:00:01 +0000

    mostly constructors

--- intrusive_optional.h
+++ intrusive_optional.h
@@ -7,66 +7,106 @@
 namespace io
 {
 
-   class bad_optional_access : public ::std::exception {
-   public:
+   struct bad_optional_access final : ::std::exception {
       [[nodiscard]] auto what() const noexcept -> const char* override
       {
          return "Bad intrusive_optional access";
       }
    };
 
+#define IO_FWD(x) static_cast<decltype(x)&&>(x)
+
+   // requires constexpr null-value and constexpr copy constructor
    template<auto null_value_param>
    struct intrusive_optional
    {
       using value_type = std::decay_t<decltype(null_value_param)>;
 
-      constexpr inline static value_type null_value = null_value_param;
+      constexpr inline static value_type null_value{ null_value_param };
       value_type m_value = null_value;
 
       constexpr intrusive_optional() noexcept = default;
+      constexpr intrusive_optional(const intrusive_optional& o) requires std::is_copy_constructible_v<value_type> = default;
+      constexpr intrusive_optional(intrusive_optional&& o) requires std::is_move_constructible_v<value_type> = default;
+
+      template<typename ... Args>
+      constexpr explicit intrusive_optional(std::in_place_t, Args&&... args) : m_value(args...)
+      {
+         
+      }
+
+      constexpr intrusive_optional(const value_type& value) : m_value(value)
+      {
+         
+      }
+
+      constexpr intrusive_optional(value_type&& value) : m_value(std::move(value))
+      {
+         
+      }
 
 
-      [[nodiscard]] constexpr auto has_value() const noexcept -> bool
-      {
-         const bool is_equal_to_null = m_value == null_value;
-         return is_equal_to_null == false;
-      }
-      [[nodiscard]] constexpr auto operator*() noexcept -> value_type&
-      {
-         return m_value;
-      }
-      [[nodiscard]] constexpr auto operator*() const noexcept -> const value_type&
-      {
-         return m_value;
-      }
-      [[nodiscard]] constexpr auto value() -> value_type&
-      {
-         if (has_value() == false)
-         {
-            throw std::bad_optional_access{};
-         }
-         return m_value;
-      }
-      [[nodiscard]] constexpr auto value() const -> const value_type&
-      {
-         if (has_value() == false)
-         {
-            throw std::bad_optional_access{};
-         }
-         return m_value;
-      }
+      [[nodiscard]] constexpr auto has_value() const noexcept -> bool;
+      [[nodiscard]] constexpr auto operator*()       noexcept ->       value_type&;
+      [[nodiscard]] constexpr auto operator*() const noexcept -> const value_type&;
+      [[nodiscard]] constexpr auto value()       ->       value_type&;
+      [[nodiscard]] constexpr auto value() const -> const value_type&;
    };
 
 
    template <class T>
    constexpr auto make_optional(T&& v) {
-      return intrusive_optional<std::decay_t<T>>{SWL_FWD(v)};
+      return intrusive_optional<std::decay_t<T>>{IO_FWD(v)};
    }
 
    template <class T, class... Args>
    constexpr auto make_optional(Args&&... args) {
-      return intrusive_optional<T>{std::in_place, SWL_FWD(args)...};
+      return intrusive_optional<T>{std::in_place, IO_FWD(args)...};
    }
 
 
+}
+
+
+template <auto null_value_param>
+constexpr auto io::intrusive_optional<null_value_param>::has_value() const noexcept -> bool
+{
+   const bool is_equal_to_null = m_value == null_value;
+   return is_equal_to_null == false;
+}
+
+
+template <auto null_value_param>
+constexpr auto io::intrusive_optional<null_value_param>::operator*() noexcept -> value_type&
+{
+   return m_value;
+}
+
+
+template <auto null_value_param>
+constexpr auto io::intrusive_optional<null_value_param>::operator*() const noexcept -> const value_type&
+{
+   return m_value;
+}
+
+
+template <auto null_value_param>
+constexpr auto io::intrusive_optional<null_value_param>::value() -> value_type&
+{
+   if (has_value() == false)
+   {
+      throw bad_optional_access{};
+   }
+   return m_value;
+}
+
+
+template <auto null_value_param>
+constexpr auto io::intrusive_optional<null_value_param>::value() const -> const value_type&
+{
+   if (has_value() == false)
+   {
+      throw bad_optional_access{};
+   }
+   return m_value;
 }
