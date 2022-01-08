21070af6a3d44bd4a87dc71cfcfeb51ac1c606b4
Sebastian Werhausen <swerhausen@gmail.com>
2022-01-08 00:00:01 +0000

    ++order

--- intrusive_optional.h
+++ intrusive_optional.h
@@ -25,6 +25,7 @@ namespace io
       value_type m_value = null_value;
    public:
 
+      // Constructors
       constexpr intrusive_optional() noexcept = default;
       constexpr intrusive_optional(const intrusive_optional& o) requires std::is_copy_constructible_v<value_type> = default;
       constexpr intrusive_optional(intrusive_optional&& o) requires std::is_move_constructible_v<value_type> = default;
@@ -32,25 +33,40 @@ namespace io
       template<typename ... Args>
       constexpr explicit intrusive_optional(std::in_place_t, Args&&... args) : m_value(args...)
       {
-         
+
       }
 
       constexpr intrusive_optional(const value_type& value) : m_value(value)
       {
-         
+
       }
 
       constexpr intrusive_optional(value_type&& value) : m_value(std::move(value))
       {
-         
+
       }
+
+
+
+      // Destructors
+      constexpr ~intrusive_optional() requires std::is_trivially_destructible_v<value_type> = default;
+
+      constexpr ~intrusive_optional() requires (std::is_trivially_destructible_v<value_type> == false)
+      {
+         if (m_value == false)
+            return;
+         m_value.~value_type();
+      }
+
+
 
       static constexpr bool has_move_assign = std::is_move_assignable_v<value_type> && std::is_move_constructible_v<value_type>;
       static constexpr bool has_trivial_move_assign = std::is_trivially_move_assignable_v<value_type> && std::is_trivially_move_constructible_v<value_type>;
       static constexpr bool has_trivial_copy = std::is_trivially_copy_assignable_v<value_type> && std::is_trivially_copy_constructible_v<value_type>;
       static constexpr bool has_copy = std::is_copy_assignable_v<value_type> && std::is_copy_constructible_v<value_type>;
 
-      constexpr intrusive_optional& operator=(const intrusive_optional& o) requires has_trivial_copy= default;
+      // operator=
+      constexpr intrusive_optional& operator=(const intrusive_optional& o) requires has_trivial_copy = default;
       constexpr intrusive_optional& operator=(const intrusive_optional& o) requires (has_copy && has_trivial_copy == false)
       {
          this->m_value = o.m_value;
@@ -81,26 +97,135 @@ namespace io
       //    // return *this;
       // }
 
-      constexpr ~intrusive_optional() requires std::is_trivially_destructible_v<value_type> = default;
 
-      constexpr ~intrusive_optional() requires (std::is_trivially_destructible_v<value_type> == false)
+
+      // Observers: operator->
+      [[nodiscard]] constexpr auto operator->()->value_type*
       {
-         if (m_value == false)
-            return;
-         m_value.~value_type();
+         return ::std::addressof(this->m_value);
+      }
+
+      [[nodiscard]] constexpr auto operator->() const -> const value_type*
+      {
+         return ::std::addressof(this->m_value);
       }
 
 
 
+      // Observers: operator*
+      [[nodiscard]] constexpr auto operator*() const& -> const value_type&
+      {
+         return this->m_value;
+      }
+
+      [[nodiscard]] constexpr auto operator*() & ->value_type&
+      {
+         return this->m_value;
+      }
+
+      [[nodiscard]] constexpr auto operator*() && ->value_type&&
+      {
+         return ::std::move(this->m_value);
+      }
+
+      [[nodiscard]] constexpr auto operator*() const&& -> const value_type&&
+      {
+         return ::std::move(this->m_value);
+      }
+
+
+
+      // Observers: operator bool
+      explicit constexpr operator bool() const noexcept
+      {
+         return this->has_value();
+      }
+
+
+
+      // Observers: has_value
+      [[nodiscard]] constexpr auto has_value() const noexcept -> bool
+      {
+         const bool is_equal_to_null = this->m_value == null_value;
+         return is_equal_to_null == false;
+      }
+
+
+
+      // Observers: value
+      [[nodiscard]] constexpr auto value() const& -> const value_type&
+      {
+         if (this->has_value == false)
+         {
+            throw bad_optional_access{};
+         }
+         return this->m_value;
+      }
+
+      [[nodiscard]] constexpr auto value() & ->value_type&
+      {
+         if (this->has_value == false)
+         {
+            throw bad_optional_access{};
+         }
+         return this->m_value;
+      }
+
+      [[nodiscard]] constexpr auto value() && ->value_type&&
+      {
+         if (this->has_value == false)
+         {
+            throw bad_optional_access{};
+         }
+         return ::std::move(this->m_value);
+      }
+
+      [[nodiscard]] constexpr auto value() const&& -> const value_type&&
+      {
+         if (this->has_value() == false)
+         {
+            throw bad_optional_access{};
+         }
+         return ::std::move(this->m_value);
+      }
+
+
+
+      // Observers: value_or
+      template <class U> requires (std::is_copy_constructible_v<value_type>&& std::is_convertible_v<U&&, value_type>)
+         [[nodiscard]] constexpr auto value_or(U&& right) const& -> value_type
+      {
+         if (this->has_value())
+         {
+            return this->m_value;
+         }
+
+         return static_cast<value_type>(::std::forward<U>(right));
+      }
+
+      template <class U> requires (std::is_move_constructible_v<value_type>&& std::is_convertible_v<U&&, value_type>)
+         [[nodiscard]] constexpr auto value_or(U&& right) && -> value_type
+      {
+         if (this->has_value())
+         {
+            return ::std::move(this->m_value);
+         }
+
+         return static_cast<value_type>(::std::forward<U>(right));
+      }
+
+
+
+      // Modifiers: swap
       constexpr void swap(intrusive_optional& right)
-         noexcept(std::is_nothrow_move_constructible_v<value_type> && std::is_nothrow_swappable_v<value_type>)
+         noexcept(std::is_nothrow_move_constructible_v<value_type>&& std::is_nothrow_swappable_v<value_type>)
          requires std::is_move_constructible_v<value_type>
       {
-         if(this->has_value() == false && right.has_value() == false)
+         if (this->has_value() == false && right.has_value() == false)
          {
             return;
          }
-         if(this->has_value() && right.has_value())
+         if (this->has_value() && right.has_value())
          {
             ::std::swap(this->m_value, right.m_value);
             return;
@@ -113,46 +238,25 @@ namespace io
 
 
 
-      template <class U> requires (std::is_copy_constructible_v<value_type> && std::is_convertible_v<U&&, value_type>)
-      [[nodiscard]] constexpr auto value_or(U&& right) const & -> value_type
+      // Modifiers: reset
+      constexpr auto reset() noexcept -> void
       {
-         if (this->has_value())
+         if (this->has_value() == false)
          {
-            return this->m_value;
+            return;
          }
 
-         return static_cast<value_type>(::std::forward<U>(right));
-      }
-
-      template <class U> requires (std::is_move_constructible_v<value_type> && std::is_convertible_v<U&&, value_type>)
-      [[nodiscard]] constexpr auto value_or(U&& right) && -> value_type
-      {
-         if (this->has_value())
+         if constexpr (std::is_trivially_destructible_v<value_type> == false)
          {
-            return ::std::move(this->m_value);
+            this->m_value.~value_type();
          }
-
-         return static_cast<value_type>(::std::forward<U>(right));
+         this->m_value = null_value;
       }
 
+      
 
-
-
-      [[nodiscard]] constexpr auto value() const &  -> const value_type&;
-      [[nodiscard]] constexpr auto value()       &  ->       value_type&;
-      [[nodiscard]] constexpr auto value()       && ->       value_type&&;
-      [[nodiscard]] constexpr auto value() const && -> const value_type&&;
-
-      [[nodiscard]] constexpr auto operator*() const &  -> const value_type&;
-      [[nodiscard]] constexpr auto operator*()       &  ->       value_type&;
-      [[nodiscard]] constexpr auto operator*()       && ->       value_type&&;
-      [[nodiscard]] constexpr auto operator*() const && -> const value_type&&;
-
-      explicit constexpr operator bool() const noexcept;
-                    constexpr auto reset() noexcept -> void;
-      [[nodiscard]] constexpr auto has_value() const noexcept -> bool;
-      [[nodiscard]] constexpr auto operator->()       ->       value_type*;
-      [[nodiscard]] constexpr auto operator->() const -> const value_type*;
+      // Modifiers: emplace
+      // TODO
    };
 
 
@@ -171,132 +275,11 @@ namespace io
       constexpr void swap(intrusive_optional<null_value>& x, intrusive_optional<null_value>& y)
       noexcept(noexcept(x.swap(y)))
    {
-      // x.swap(y);
+      x.swap(y);
    }
 
 
 } // namespace io
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::operator->() -> value_type*
-{
-   return ::std::addressof(this->m_value);
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::operator->() const -> const value_type*
-{
-   return ::std::addressof(this->m_value);
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::value() const & -> const value_type&
-{
-   if (this->has_value == false)
-   {
-      throw bad_optional_access{};
-   }
-
-   return this->m_value;
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::value() & -> value_type&
-{
-   if (this->has_value == false)
-   {
-      throw bad_optional_access{};
-   }
-
-   return this->m_value;
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::value() && -> value_type&&
-{
-   if (this->has_value == false)
-   {
-      throw bad_optional_access{};
-   }
-
-   return ::std::move(this->m_value);
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::value() const && -> const value_type&&
-{
-   if (this->has_value() == false)
-   {
-      throw bad_optional_access{};
-   }
-
-   return ::std::move(this->m_value);
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::operator*() const & -> const value_type&
-{
-   return this->m_value;
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::operator*() & -> value_type&
-{
-   return this->m_value;
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::operator*() && -> value_type&&
-{
-   return ::std::move(this->m_value);
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::operator*() const && -> const value_type&&
-{
-   return ::std::move(this->m_value);
-}
-
-
-template <auto null_value_param>
-constexpr io::intrusive_optional<null_value_param>::operator bool() const noexcept
-{
-   return this->has_value();
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::reset() noexcept -> void
-{
-   if (this->has_value() == false)
-   {
-      return;
-   }
-
-   if constexpr (std::is_trivially_destructible_v<value_type> == false)
-   {
-      this->m_value.~value_type();
-   }
-   this->m_value = null_value;
-}
-
-
-template <auto null_value_param>
-constexpr auto io::intrusive_optional<null_value_param>::has_value() const noexcept -> bool
-{
-   const bool is_equal_to_null = this->m_value == null_value;
-   return is_equal_to_null == false;
-}
 
 
 // TODO hash
